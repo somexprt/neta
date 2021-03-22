57a99de80c23771a1a834c8b426cdb1a2b15f7de
Alexander Mashkov <57066216+somexprt@users.noreply.github.com>
2021-03-22 08:26:31 +0300

    0.5.1 - Total Rebuild
    After the release of Ghost 4.0, Neta was redesigned from scratch, taking as a basis the updated version of Casper Theme. Grids, flexboxes, and other modern things.

--- assets/js/global.js
+++
@@ -1,7 +0,0 @@
-var doubleHover = function(selector, hoverClass) {
-  $(document).on('mouseover mouseout', selector, function(e) {
-      $(selector)
-      .filter('[href="' + $(this).attr('href') + '"]')
-      .toggleClass(hoverClass, e.type == 'mouseover');
-  });
-}
